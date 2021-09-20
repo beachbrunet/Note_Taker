5acb2ffee303613f4c9ff48bdca0f9dbfc3a08de
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-19 19:15:29 -0600

    working on api route

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,14 +1,6 @@
 //  add dependancies
-const fs = require("fs");
-const path = require("/db/db.json");
-
-const PORT = 3001;
-const app = express();
-
-//  Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const path = require("path");
+const get = require("./db/db.json");
 
 // ROUTES (to get notes, create notes, then delete)-------------------------------------------------
 
