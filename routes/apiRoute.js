154e4e7d41d91582d26c8c0fee1d69d49d79321e
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-19 20:17:03 -0600

    upodate

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -30,4 +30,22 @@ fs.writeFileSync(
   //   err ? console.error(err) : console.info(`\nData written to ${destination}`)
   // );
 );
+
+// get routes
+// app.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public/index.html"))
+// );
+
+// app.get("/notes", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public/index.html"))
+// );
+
+
+
+
+
+
+
+
+
 // module.exports = app;
